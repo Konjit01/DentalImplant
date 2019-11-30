5b1c040536a5a622ad6edf17840981f472880857
KonjitWeldesenbet <48951542+Konjit01@users.noreply.github.com>
2019-11-30 18:30:49 +0100

    Update DentalImplantMacro.py

--- DentalImplantMacro.py
+++ DentalImplantMacro.py
@@ -835,46 +835,6 @@ def encryption(privateInfo):
   encoded = EncodingAES(cipher, privateInfo)
   print 'Encrypted String', encoded
   
-  
-def derive_key_and_iv(password, salt, key_length, iv_length):
-  d = d_i = ''
-  while len(d) < key_length + iv_length:
-    d_i = md5(d_i + password + salt).digest()
-    d += d_i
-  return d[:key_length], d[key_length:key_length+iv_length]
-
-def encrypt(out_file, password, key_length=32):
-    bs = AES.block_size
-    salt = Random.new().read(bs - len('Salted__'))
-    key, iv = derive_key_and_iv(password, salt, key_length, bs)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    out_file.write('Salted__' + salt)
-    finished = False
-    while not finished:
-        chunk = 1024 * bs
-        if len(chunk) == 0 or len(chunk) % bs != 0:
-            padding_length = (bs - len(chunk) % bs) or bs
-            chunk += padding_length * chr(padding_length)
-            finished = True
-        out_file.write(cipher.encrypt(chunk))
-
-def decrypt(in_file, out_file, password, key_length=32):
-    bs = AES.block_size
-    salt = in_file.read(bs)[len('Salted__'):]
-    key, iv = derive_key_and_iv(password, salt, key_length, bs)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    next_chunk = ''
-    finished = False
-    while not finished:
-        chunk, next_chunk = next_chunk, cipher.decrypt(in_file.read(1024 * bs))
-        if len(next_chunk) == 0:
-            padding_length = ord(chunk[-1])
-            chunk = chunk[:-padding_length]
-            finished = True
-        out_file.write(chunk)
-
-
-
 def applyCropToSelectedRegion(field):
   ctx.field("cropImageInXYZ.apply").touch()
   return
